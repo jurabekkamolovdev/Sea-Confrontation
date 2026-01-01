ecc7340df733b5cd6ae0e2a8d4857dde6c23d569
jurabekkamolovdev <jurabek.dev101@gmail.com>
2026-01-01 16:33:25 +0500

    [+] Ai palyer init

---
+++ exercise6/exercise-6.js
@@ -0,0 +1,30 @@
+import { Player } from "../exercise3/exercise-3.js";
+
+class AIPlayer extends Player {
+    constructor(name, boardSize) {
+        super(name, boardSize);
+    }
+
+    placeShips(shipName, length, isVertical, startPosition) {
+        const availableCells = this._board.findAvailableCells();
+
+        const randomIndex = Math.floor(Math.random() * availableCells.length);
+        const start = availableCells[randomIndex];
+
+        const vertical = Number(Math.random() > 0.5);
+
+        super.placeShips(shipName, length, start, vertical);
+    }
+
+    takeTurn(opponent) {
+        const x = Math.floor(Math.random() * this._boardSize);
+        const y = Math.floor(Math.random() * this._boardSize);
+
+        return {
+            opponent, x, y
+        }
+    }
+}
+
+const aiPlayer = new AIPlayer("Ai", 10);
+console.log(aiPlayer.name);
